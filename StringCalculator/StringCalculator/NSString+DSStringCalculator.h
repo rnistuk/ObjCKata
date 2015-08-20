d5d708701cbbda32fddef3362f299f32c053ecee
Rich Nistuk <rnistuk@Richs-MacBook-Pro.local>
2015-08-19 18:15:53 -0700

    Refactored out some mistakes that I had made w.r.t. import directives, and removed some cruft.

--- StringCalculator/StringCalculator/NSString+DSStringCalculator.h
+++ StringCalculator/StringCalculator/NSString+DSStringCalculator.h
@@ -1,11 +1,3 @@
-//
-//  NSString+DSStringCalculator.h
-//  StringCalculator
-//
-//  Created by Rich Nistuk on 2015-08-19.
-//  Copyright (c) 2015 Rich Nistuk. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface NSString (DSStringCalculator)
--- StringCalculator/StringCalculator/NSString+DSStringCalculator.m
+++ StringCalculator/StringCalculator/NSString+DSStringCalculator.m
@@ -1,11 +1,3 @@
-//
-//  NSString+DSStringCalculator.m
-//  StringCalculator
-//
-//  Created by Rich Nistuk on 2015-08-19.
-//  Copyright (c) 2015 Rich Nistuk. All rights reserved.
-//
-
 #import "NSString+DSStringCalculator.h"
 
 @implementation NSString (DSStringCalculator)
