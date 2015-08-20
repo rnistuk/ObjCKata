408b090aecf2d046f713141727b3c19ee7a944ff
Rich Nistuk <rnistuk@Richs-MacBook-Pro.local>
2015-08-19 18:41:26 -0700

    Added the failing test for item 4.: Support different delimiters.

--- StringCalculator/Tests/Tests.m
+++ StringCalculator/Tests/Tests.m
@@ -42,5 +42,14 @@
     XCTAssertEqual(6, [NSString Add:@"1\n2,3"]);
 }
 
+- (void)testSupportDifferentDelimiters {
+    // to change a delimiter, the beginning of the string will contain a
+    // separate line that looks like this: “//[delimiter]\n[numbers…]” for
+    // example “//;\n1;2” should return three where the default delimiter is ‘;’ .
+    // the first line is optional. all existing scenarios should still be
+    // supported
+    XCTAssertEqual(3, [NSString Add:@"//;\n1;2"]);
+}
+
 
 @end
