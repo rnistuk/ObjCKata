bb89ea1607ed099a39849f5fe18fbe23ea878d97
Rich Nistuk <rnistuk@Richs-MacBook-Pro.local>
2015-08-19 18:19:36 -0700

    Added a test for adding 0 and 1 numbers.

--- StringCalculator/Tests/Tests.m
+++ StringCalculator/Tests/Tests.m
@@ -21,4 +21,13 @@
     XCTAssert([NSString respondsToSelector:@selector(Add:)], @"Pass");
 }
 
+- (void)testAddUpToThreeIntegers {
+    // The method can take 0, 1 or 2 numbers, and will return their sum (for an
+    // empty string it will return 0) for example “” or “1” or “1,2”
+    XCTAssertEqual(0, [NSString Add:@""]);
+    XCTAssertEqual(1, [NSString Add:@"1"]);
+}
+
+
+
 @end
